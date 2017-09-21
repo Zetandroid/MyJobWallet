f7ac4e2d727015f7df612cde727ea98dba12a1d5
mowmo <mowwmo@gmail.com>
2017-09-21 15:41:05 +0200

    Aggiunto Login Google

--- app/src/main/java/com/kubix/myjobwallet/MainActivity.java
+++ app/src/main/java/com/kubix/myjobwallet/MainActivity.java
@@ -1,6 +1,7 @@
 package com.kubix.myjobwallet;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -13,9 +14,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
+
+import com.kubix.myjobwallet.adapter.GridHomeAdapter;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    //TODO GRID HOME
+    GridView grid;
+    String[] web = {
+            "CALENDARIO ORE",
+            "TURNI",
+            "SPESE DEL MESE",
+            "STORIA",
+    } ;
+    int[] imageId = {
+            R.drawable.grid_image1,
+            R.drawable.grid_image2,
+            R.drawable.grid_image3,
+            R.drawable.grid_image4,
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +44,29 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
+        //TODO GRIGLIA HOME
+        GridHomeAdapter adapter = new GridHomeAdapter(MainActivity.this, web, imageId);
+        grid=(GridView)findViewById(R.id.gridHome);
+        grid.setAdapter(adapter);
+        grid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                Toast.makeText(MainActivity.this, "You Clicked at " +web[+ position], Toast.LENGTH_SHORT).show();
+                //TODO Apertura Orario
+                startActivity(new Intent(MainActivity.this,SettingActivity.class));
+
+                //TODO Apertura Turni
+                startActivity(new Intent(MainActivity.this,SettingActivity.class));
+
+                //TODO Apertura Spese
+
+                //TODO Apertura Storia
+
+            }
+        });
 
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
