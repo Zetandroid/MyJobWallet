751b38b6ca539ec5b8fab91ffdb5fca4067d29bf
mowmo <mowwmo@gmail.com>
2017-09-21 11:24:34 +0200

    caricamento

---
+++ app/src/main/java/com/kubix/myjobwallet/ProfileActivity.java
@@ -0,0 +1,17 @@
+package com.kubix.myjobwallet;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by mowmo on 21/09/17.
+ */
+
+ public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
---
+++ app/src/main/java/com/kubix/myjobwallet/SettingActivity.java
@@ -0,0 +1,18 @@
+package com.kubix.myjobwallet;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by mowmo on 21/09/17.
+ */
+
+public class SettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+    }
+}
+
