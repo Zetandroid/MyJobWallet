52ebb3bdcc2b915a00c69b27501439b6d4bb1102
mowmo <mowwmo@gmail.com>
2017-10-01 20:17:13 +0200

    aggiunto menu' "spese" aggiunta icona app modificato icone

--- app/src/main/java/com/kubix/myjobwallet/SpeseActivity.java
+++ app/src/main/java/com/kubix/myjobwallet/SpeseActivity.java
@@ -1,7 +1,10 @@
 package com.kubix.myjobwallet;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 /**
  * Created by mowmo on 26/09/17.
@@ -12,6 +15,15 @@ public class SpeseActivity extends AppCompatActivity{
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ore);
+        setContentView(R.layout.activity_spese);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.floatingAdd);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity (new Intent(SpeseActivity.this,SpeseAggiungiActivity.class));
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/kubix/myjobwallet/SpeseAggiungiActivity.java
@@ -0,0 +1,16 @@
+package com.kubix.myjobwallet;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by mowmo on 30/09/17.
+ */
+
+public class SpeseAggiungiActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spese_aggiungi);
+    }
+}
