48d1a71ed5cbb95ee603d8dbb4425a953118de2d
mowmo <mowwmo@gmail.com>
2017-10-06 21:24:19 +0200

    Modifica colori Layout Modifica layout profilo Modifica icone

--- app/src/main/java/com/kubix/myjobwallet/SettingActivity.java
+++ app/src/main/java/com/kubix/myjobwallet/SettingActivity.java
@@ -1,6 +1,7 @@
 package com.kubix.myjobwallet;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.support.v7.app.ActionBar;
 import android.support.v7.widget.Toolbar;
 
 public class SettingActivity extends PreferenceActivity {
@@ -9,6 +10,8 @@ public class SettingActivity extends PreferenceActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.impostazioni);
+
+
     }
 
 }
