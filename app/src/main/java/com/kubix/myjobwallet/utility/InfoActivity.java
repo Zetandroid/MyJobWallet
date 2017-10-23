496d5730c40a8a176c2657754991487fbaa03b97
mowmo <mowwmo@gmail.com>
2017-10-23 06:20:03 +0200

    Login Google Aggiunto Link in "Info" Modifica dei bottoni Altro

--- app/src/main/java/com/kubix/myjobwallet/utility/InfoActivity.java
+++ app/src/main/java/com/kubix/myjobwallet/utility/InfoActivity.java
@@ -1,10 +1,16 @@
 package com.kubix.myjobwallet.utility;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 
 import com.kubix.myjobwallet.R;
+
+import static com.google.android.gms.R.id.button;
 
 /**
  * Created by mowmo on 16/10/17.
@@ -22,5 +28,36 @@ public class InfoActivity extends AppCompatActivity {
         setTitle(R.string.toolbarInfo);
         toolbar.setTitleTextColor(getResources().getColor(R.color.testoBianco));
         setSupportActionBar(toolbar);
+
+        Button googleMorgan = (Button) findViewById(R.id.googleMorgan);
+        Button twitterMorgan = (Button) findViewById(R.id.twitterMorgan);
+        Button googlelessio = (Button) findViewById(R.id.googleAlessio);
+        Button twitterAlessio = (Button) findViewById(R.id.twitterAlessio);
+
+        googleMorgan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://plus.google.com/u/0/+MOWMO")));
+            }
+        });
+        twitterMorgan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Intent.ACTION_VIEW,Uri.parse("https://twitter.com/_mowmo_")));
+            }
+        });
+
+        googlelessio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Intent.ACTION_VIEW,Uri.parse("https://plus.google.com/u/0/108762755836299160559")));
+            }
+        });
+        twitterAlessio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Intent.ACTION_VIEW,Uri.parse("")));
+            }
+        });
     }
 }
