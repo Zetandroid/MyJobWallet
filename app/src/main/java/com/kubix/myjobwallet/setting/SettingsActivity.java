06126ad6295eab98924bf93d7a8dfcc6612c4c76
Walter <walter.tomiati.90@gmail.com>
2017-12-11 19:11:44 +0100

    Night Mode
    Night Mode fixed

--- app/src/main/java/com/kubix/myjobwallet/setting/SettingsActivity.java
+++ app/src/main/java/com/kubix/myjobwallet/setting/SettingsActivity.java
@@ -19,11 +19,17 @@ public class SettingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbarSetting);
-        setTitle(R.string.toolbarSettings);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        try {
+            Toolbar toolbar = (Toolbar) findViewById(R.id.toolbarSetting);
+            setTitle(R.string.toolbarSettings);
+            setSupportActionBar(toolbar);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
+        catch (Exception e) {
+            setTheme(R.style.AppTheme_Dark);
+        }
+
 
         getFragmentManager().beginTransaction().replace(R.id.fragment_container, new SettingsFragment()).commit();
     }
